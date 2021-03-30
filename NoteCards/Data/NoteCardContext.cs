64ef19ec81cddbacba85e0c0196cfbf7aa8025a8
Peter Kang <pxk7062@rit.edu>
2021-03-30 19:53:10 -0400

    Created A Note Card Context

---
+++ NoteCards/Data/NoteCardContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Threading.Tasks;
+using NoteCards.Models;
+
+namespace challenge.Data
+{
+    public class NoteCardContext : DbContext
+    {
+        public NoteCardContext() : base("NoteCardContext")
+        {
+
+        }
+
+        public DbSet<NoteCard> NoteCards { get; set; }
+    }
+}
